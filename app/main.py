5453765c6ed8489781a57e42ce26e6122a5a0190
Teashrock <kajitsu22@gmail.com>
2026-06-24 00:23:28 +0300

    Implemented descriptive variable names

--- app/main.py
+++ app/main.py
@@ -8,10 +8,10 @@ def calculate_team_total_rating(players: list[Player]) -> int:
 
 
 def elves_concert(elves: list[Elf]) -> None:
-    for elf in elves:
-        elf.play_elf_song()
+    for elf_player in elves:
+        elf_player.play_elf_song()
 
 
 def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
-    for dwarf in dwarves:
-        dwarf.eat_favourite_dish()
+    for dwarf_player in dwarves:
+        dwarf_player.eat_favourite_dish()
